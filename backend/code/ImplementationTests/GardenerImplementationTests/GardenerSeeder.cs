b507ab1d7dc264d9b56e32e6d210912340282473
kacengas <kacengas@gmail.com>
2025-04-28 17:33:16 +0200

    gardener tests init

---
+++ backend/code/ImplementationTests/GardenerImplementationTests/GardenerSeeder.cs
@@ -0,0 +1,34 @@
+﻿using DTOs;
+using Entities;
+
+namespace ImplementationTests.GardenerImplementationTests;
+
+public static class GardenerSeeder
+{
+    public static async Task<Gardener> SeedGardenerAsync()
+    {
+        var context = TestSetup.Context;
+        
+        var gardener = new Gardener
+        {
+            Username = "testGardener",
+            Password = "testPassword"
+        };
+        
+        await context.Gardeners.AddAsync(gardener);
+        await context.SaveChangesAsync();
+
+        return gardener;
+    }
+    
+    public static GardenerDTO SeedGardenerDto()
+    {
+        var gardenerDto = new GardenerDTO
+        {
+            Username = "testGardener",
+            Password = "testPassword"
+        };
+
+        return gardenerDto;
+    }
+}
---
+++ backend/code/ImplementationTests/TestSetup.cs
@@ -0,0 +1,39 @@
+﻿using Database;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ImplementationTests;
+
+[SetUpFixture]
+public static class TestSetup
+{
+    private static AppDbContext _context;
+
+    public static AppDbContext Context
+    {
+        get => _context;
+    }
+
+    [OneTimeSetUp]
+    public static void GlobalSetup()
+    {
+        var services = new ServiceCollection();
+
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseInMemoryDatabase("TestDb")
+        );
+
+        services.AddScoped<DbContext, AppDbContext>();
+
+        var provider = services.BuildServiceProvider();
+        _context = provider.GetRequiredService<AppDbContext>();
+    }
+    
+    [OneTimeTearDown]
+    public static void GlobalTearDown()
+    {
+        _context?.Dispose();
+        _context = null;
+    }
+}
