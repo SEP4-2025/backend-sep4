180f0f015292da21bb58a95f751d9ca77b0687c1
kacengas <kacengas@gmail.com>
2025-05-15 16:57:24 +0200

    port change

--- backend/code/Server/WebAPI/Program.cs
+++ backend/code/Server/WebAPI/Program.cs
@@ -15,6 +15,8 @@ using WebAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 // Add services to the container.
 
 builder.Services.AddControllers();
