ddd03764ea8cf791d5cecddbf3a1faa34e55157a
Mario <344345@via.dk>
2025-05-19 23:01:15 +0200

    create endpoints to get the water humidity delta
    created relevant dtos, modified the prediction entity and modified controlle, logic and interface

---
+++ backend/code/Shared/DTOs/PredictionRequestDTO.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DTOs;
+
+public class PredictionRequestDTO
+{
+    [JsonPropertyName("temperature")]
+    public double Temperature { get; set; }
+
+    [JsonPropertyName("light")]
+    public double Light { get; set; }
+
+    [JsonPropertyName("airHumidity")]
+    public double AirHumidity { get; set; }
+
+    [JsonPropertyName("soilHumidity")]
+    public double SoilHumidity { get; set; }
+
+    [JsonPropertyName("date")]
+    public DateTime Date { get; set; }
+
+    [JsonPropertyName("greenhouseId")]
+    public int GreenhouseId { get; set; }
+
+    [JsonPropertyName("sensorReadingId")]
+    public int SensorReadingId { get; set; }
+}
